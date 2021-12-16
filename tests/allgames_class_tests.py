196543a29364499842a0772ab5c3f6d5790327dc
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2021-12-16 22:34:24 +0100

    create tests files

---
+++ tests/allgames_class_tests.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('./')
+from packages import *
+
+from src.all_games import Game, AllGames
---
+++ tests/playgame_class_tests.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('./')
+from packages import *
+
+from src.play import PlayGame
