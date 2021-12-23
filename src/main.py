8d32a8a060a7cdf3241ed72b671faa9fae2a33b9
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2021-12-24 00:03:11 +0100

    add files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('./')
 from packages import *
-from all_games import AllGames
+from src.all_games import AllGames
 
 
 def main():
@@ -12,7 +12,10 @@ def main():
         all_games = AllGames(size)
         with open(f'src/games/all_games_{size}.pickle', 'wb') as file:
             pickle.dump(all_games, file)
-    
+
+        with open(f'tests/resources/all_games_{size}.txt','w+') as help:
+            help.writelines('%s\n' % json.dumps(line) for line in list(map(lambda game: str(list(game.values())[0].get_game_result()) ,all_games.get_all_games())))
+            
 
 if __name__=="__main__":
     main()
